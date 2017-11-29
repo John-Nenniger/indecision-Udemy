794e2ba17899d81634e3912e0fae7d169e2c26e2
John Nenniger <JPNENNIGER@GMAIL.com>
2017-11-29 15:47:50 -0800

    second arrow function challenge

--- src/playground/es6-arrow-functions2.js
+++ src/playground/es6-arrow-functions2.js
@@ -1,1 +1,19 @@
 //arguments object
+
+
+// arrow functions don't bind their this to the current function, it is hoisted
+
+const multiplier = {
+  numbers: [1,3,4,5,27,89],
+  multiplyBy: 4,
+  multiply(){
+    return this.numbers.map((number) => number * this.multiplyBy);
+  }
+};
+
+console.log(multiplier.multiply())
+
+
+
+
+console.log('donzo')
