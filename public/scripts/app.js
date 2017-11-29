aa47b9b3a25268360f100502a3e5d72ceb443214
John Nenniger <JPNENNIGER@GMAIL.com>
2017-11-28 19:33:14 -0800

    lecture 12 one way

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,50 +2,81 @@
 
 console.log("app.js is running");
 
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      "p",
+      null,
+      " Location: ",
+      location,
+      " "
+    );
+  } else {
+    return undefined;
+  }
+}
+
+function createOptions(options) {
+  if (options.length > 0) {
+    return "Here are your options: " + options;
+  } else {
+    return "No options";
+  }
+}
+
+var app = {
+  title: "Spork",
+  subtitle: "cutlery?",
+  options: ['One', 'Two']
+};
+
+console.log(app.subtitle);
 var template = React.createElement(
   "div",
   null,
   React.createElement(
     "h1",
     null,
-    "This is JSX rendered by app.js"
+    app.title
+  ),
+  true && React.createElement(
+    "p",
+    null,
+    app.subtitle
   ),
   React.createElement(
     "p",
     null,
-    "sup"
+    createOptions(app.options)
   )
 );
 
 var user = {
-  name: "John Nenniger",
+  name: "John",
   age: 25,
   location: "Vancouver"
 };
 
-var userName = "John Nenniger";
-var userAge = 25;
-var userLocation = "Vancouver, Canada";
 var templateTwo = React.createElement(
   "div",
   null,
   React.createElement(
     "h1",
     null,
-    user.name
+    user.name ? user.name : "Anonymous"
   ),
-  React.createElement(
+  false,
+  user.age && user.age >= 18 && React.createElement(
     "p",
     null,
     "Age: ",
     user.age
   ),
-  React.createElement(
-    "p",
-    null,
-    "Location: ",
-    user.location
-  )
+  getLocation(user.location)
 );
+
+// ternary operator condition ? "what happens if true" : "what happens if false"
+
+
 var appRoot = document.getElementById('app');
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,28 +1,55 @@
 
 console.log("app.js is running")
 
+function getLocation(location){
+  if(location){
+    return <p> Location: {location} </p>
+  } else {
+    return undefined
+  }
+}
+
+function createOptions(options){
+  if (options.length>0){
+    return `Here are your options: ${options}`
+  } else {
+    return "No options"
+  }
+}
+
+let app = {
+  title: "Spork",
+  subtitle: "cutlery?",
+  options: ['One', 'Two']
+}
+
+console.log(app.subtitle)
 var template = (
 <div>
-  <h1>This is JSX rendered by app.js</h1>
-  <p>sup</p>
+  <h1>{app.title}</h1>
+  {true && <p>{app.subtitle}</p>}
+  <p>{createOptions(app.options)}</p>
 </div>
 );
 
 let user = {
-  name: "John Nenniger",
+  name: "John",
   age: 25,
   location: "Vancouver"
 }
 
-let userName = "John Nenniger";
-let userAge = 25;
-let userLocation = "Vancouver, Canada"
 var templateTwo = (
   <div>
-    <h1>{user.name}</h1>
-    <p>Age: {user.age}</p>
-    <p>Location: {user.location}</p>
+    <h1>{user.name ?  user.name : "Anonymous"}</h1>
+    {/* booleans, Null, and undefined are all ignored by JSX */}
+    {false}
+    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
   </div>
 )
+
+// ternary operator condition ? "what happens if true" : "what happens if false"
+
+
 var appRoot = document.getElementById('app')
-ReactDOM.render(templateTwo, appRoot)
+ReactDOM.render(template, appRoot)
