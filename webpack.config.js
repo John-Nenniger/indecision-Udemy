42d939c5bf97b2e870ec1f1ac7da9c72d636231d
John Nenniger <JPNENNIGER@GMAIL.com>
2018-03-19 15:46:36 -0700

    started setting up webpack, lecture 51

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// entry -> output
+const path = require('path');
+
+console.log(__dirname)
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  }
+};
