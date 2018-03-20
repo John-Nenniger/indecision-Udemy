8a387ae3bb919547197e14e36e9df2fe348ed2fb
John Nenniger <JPNENNIGER@GMAIL.com>
2018-03-20 00:06:51 -0700

    lecture 54

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
-import thisIsSubtractButWeCanCallItWhatever, { square, add } from './utils.js';
-import isSenior, { isAdult, canDrink } from './person.js';
+// import thisIsSubtractButWeCanCallItWhatever, { square, add } from './utils.js';
+// import isSenior, { isAdult, canDrink } from './person.js';
+// import validator from 'validator';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const l = console.log
 l('app.js is running!!!!!!!!!!!');
-l(square(3))
-l(add(100, 45))
-l(isAdult(12))
-l(canDrink(23))
-l(thisIsSubtractButWeCanCallItWhatever(1242, 536))
-l(isSenior(45))
-l(isSenior(72))
+const template = React.createElement('p', {}, 'Whaddup')
+
+ReactDOM.render(template, document.getElementById('app'))
