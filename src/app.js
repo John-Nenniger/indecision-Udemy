6ddb6bfb405ac356867b7f488f26df3cc709def1
John Nenniger <JPNENNIGER@GMAIL.com>
2018-03-22 13:35:21 -0700

    lecture 67, added a css reset using normalize css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Indecision from './components/Indecision';
-import './styles/styles.scss'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss';
 
 ReactDOM.render(<Indecision options={[]} />, document.getElementById('app'))
